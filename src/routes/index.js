31f347cada9872675ad86339c10fe2c8ebf83c93
Nhu Tu <tuainhu160296@gmail.com>
2019-04-10 16:52:00 +0700

    test get list sprints

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,31 @@ const router =  express.Router();
 
 import { jira } from './../config'
 
-router.get('/:issueNumber', function(req, res, next){
+router.get('/', function(req, res, next){
     // ES6
-    jira.findIssue(req.params.issueNumber)
-    .then(issue => {
-    console.log(`Status: ${issue.fields.status.name}`);
+    jira.getAllBoards()
+    .then(data => {
+        console.log(data);
     })
     .catch(err => {
-    console.error(err);
+        console.error(err);
     });
+});
+
+router.get('/:boardId', function(req, res, next){
+    // ES6
+    jira.getBoard(req.params.boardId)
+    .then(data=>{
+        console.log(data)
+        return jira.getAllSprints(data.id)
+    })
+    .then(data2 =>{
+        console.log(data2)
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
 });
 
 module.exports = router;
