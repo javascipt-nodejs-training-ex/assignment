0652188abf4d10e2c6cf674529e96bb44a89b1cd
Nhu Tu <tuainhu160296@gmail.com>
2019-04-10 14:13:13 +0700

    add route folder

--- src/main.js
+++ src/main.js
@@ -1,27 +1,15 @@
 import express from 'express';
-import https from 'https';
+import http from 'http';
 
-import { jira } from './config'
 import routes from './routes';
 
 require('https').globalAgent.options.ca = require('ssl-root-cas').create();
-
-
-// ES6
-jira.findIssue('ER-857')
-.then(issue => {
-console.log(`Status: ${issue.fields.status.name}`);
-})
-.catch(err => {
-console.error(err);
-});
-
 const port = 3000;
 const app = express();
 
 app.use('/api', routes)
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
-const express = require('express');
+import express from 'express';
 const router =  express.Router();
 
-router.get('/:issueNumber', function(req, res, next){
+import { jira } from './../config'
 
+router.get('/:issueNumber', function(req, res, next){
+    // ES6
+    jira.findIssue(req.params.issueNumber)
+    .then(issue => {
+    console.log(`Status: ${issue.fields.status.name}`);
+    })
+    .catch(err => {
+    console.error(err);
+    });
 });
 
 module.exports = router;
