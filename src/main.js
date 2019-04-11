d0a7bb2a100e148b47492319e5162bff56158c64
Nhu Tu <tuainhu160296@gmail.com>
2019-04-11 17:02:09 +0700

    update cache api

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import http from 'http';
 import routes from './routes';
 
 require('https').globalAgent.options.ca = require('ssl-root-cas').create();
+
 const port = 3000;
 const app = express();
+const server = http.createServer(app);
 
 app.use('/api', routes);
 
-const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import apicache from 'apicache';
 
 import BoardController from './../controllers/boardController';
 import SprintController from './../controllers/sprintController';
 import MemberController from './../controllers/memberController';
 
 const route =  express.Router();
+let cache = apicache.middleware;
 
-route.get('/:boardId',BoardController.getBoard);
+route.use(cache('1 hour'))
+
+route.get('/:boardId', BoardController.getBoard);
 route.get('/:boardId/all-sprints', SprintController.getAllSprints);
 route.get('/user/:boardId/:sprintId', MemberController.getUsers);
 
